3213ae4634cdb42c4a56ab63dc3a1ef2f6a644a9
Romario Ferreira <romarioferreirarezende@hotmail.com>
2020-07-01 22:23:39 -0300

    iniciado organizacao do codigo ja feito

--- agenda_Versao_beta.cpp
+++ agenda_Versao_beta.cpp
@@ -1,37 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdbool.h>
 
+    //variaveis globais
+    const int limit_register = 1;   //limite de cadastros
+    int user_code = 0; 
+	
+    
     // estrutura de registro de clientes
-    int position_code = 0;
-    const int maximoDeClient = 5;
     struct registerClient{
 		char Name[50];
 		char Tell[20];
 		char Email[50];
 	};
      
-    
 	struct registerClient reg;
-	struct registerClient dateBook[maximoDeClient];	
+	struct registerClient dateBook[limit_register];	//agenda de 5 posicoes
 	
-	
-void titleAplication(){
-	printf("--------------- AGENDA - VERSAO BETA ----------------\n\n");
-}
 
-int menuOptions(){
-	int response;
+//imprime o titulo e menu do programa
+void menuApplication(){
+	printf("--------------- AGENDA - VERSAO BETA ----------------\n\n");
 	printf("1 - Didite 1 para inserir novo cadastro:\n");
 	printf("2 - Didite 2 para mostrar clientes cadastrados:\n");
 	printf("0 - Digite 0 para encerrar:\n\n");
+}
+
+//retorna a resposta do usuario
+int menuOptions(){
+	int response;
 	scanf("%d",&response);
 	fflush(stdin);
 	return response;
 }
 
+//recebe os dados dos clientes
 void getDataClient(){
-	
 	
 	printf("Insira o nome do Cliente:\n");
 	gets(reg.Name);
@@ -47,13 +52,30 @@ void getDataClient(){
 	
 }
 
+//limpa a tela
 void cleanScreen(){
 	system("cls");
 }
 
+//pega o registro do cliente e armazena na agenda
 void saveApointment(registerClient r){
-	dateBook[position_code] = r;
-	position_code++;
+	dateBook[user_code] = r;
+	user_code++;
+}
+
+int checkLimit(){
+	bool check = false;
+	if(user_code < limit_register){
+		printf("tem vaga");
+		check = true;
+		system("PAUSE");
+	}
+	else{
+		printf("agenda lotada");
+		check = false;
+	}
+	return check;
+	
 }
 
 
@@ -62,23 +84,21 @@ void saveApointment(registerClient r){
 int main(){
 	
 	//exibe o titulo da aplicacao
-	titleAplication();
+	menuApplication();
 	
-	//exibe o menu para o usuario e trata a selecao do comando escolhido
+	
 	switch (menuOptions()){
 		case 1:
-			
-			cleanScreen(); // limpa a tela
-			getDataClient();  //solicita os dados do cliente
-			saveApointment(reg); //salva os registro na agenda na posicao
-		
+			if(checkLimit() == true){
+				cleanScreen(); // limpa a tela
+			    getDataClient();  //solicita os dados do cliente
+			    saveApointment(reg); //salva os registro na agenda na posicao
+			}
 			
 		break;
 		
 		case 2:
-			
-			if(position_code != 0){
-				
+			if(user_code != 0){
 				printf("%s ",dateBook[0].Name);
 	            printf("%s ",dateBook[0].Email);
 	            printf("%s ",dateBook[0].Tell);
